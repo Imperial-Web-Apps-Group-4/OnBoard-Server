348b34276e39932b81392c1ca9d11c5f5843dc54
Kian Mayne <km3315@ic.ac.uk>
2017-06-15 16:40:42 +0100

    Implemented dice roll, prevented official forgery
    - Implemented a /dice command with optional side count
    - Ensured users couldn't send official messages by setting the offical flag to false for every user-submitted message

--- src/GameSession.js
+++ src/GameSession.js
@@ -16,7 +16,7 @@ class GameSession {
     connection.on('message', this.handleMessage.bind(this));
     connection.send(new Message.InitMessage('v1', this.game));
     const people_message = this.connections.length == 1 ? 'is 1 person playing' : `are ${this.connections.length} people`;
-    connection.send(new Message.ChatMessage('OnBoard', `Connected to game server. There ${people_message} playing.`, true));
+    connection.send(new ServerMessage(`Connected to game server. There ${people_message} playing.`));
   }
 
   handleMessage(connection, msgString) {
@@ -36,7 +36,13 @@ class GameSession {
       this.broadcastMessageExcluding(msg, connection);
       break;
     case 'chat':
+      // Stop users from sending official messages
+      msg.official = false;
       this.broadcastMessageExcluding(msg, connection);
+      if (msg.content[0] == '/') {
+        let res = processCommand(msg.content.substr(1), msg.name);
+        this.broadcastMessage(res);
+      }
       break;
     default:
       if (!msg.type) connection.die('Type field missing');
@@ -46,12 +52,46 @@ class GameSession {
     }
   }
 
+  broadcastMessage(msg) {
+    this.broadcastMessageExcluding(msg, null);
+  }
+
   broadcastMessageExcluding(msg, connection) {
     this.connections.forEach(client => {
-      if (client != connection && client.live()) {
+      if (client !== connection && client.live()) {
         client.send(msg);
       }
     });
+  }
+}
+
+function processCommand(msgString, username) {
+  let command = msgString.split(' ');
+  switch (command[0]) {
+  case 'r':
+  case 'roll':
+    return rollDieCommand(username, command[1]);
+  default:
+    return new ServerMessage('Command not recognised.');
+  }
+}
+
+function rollDieCommand(username, arg) {
+  let sides = 6;
+  let sided = '';
+  if (arg) {
+    sides = parseInt(arg);
+    sided = `${sides}-sided `;
+    if (isNaN(sides)) return new ServerMessage('Invalid sides option. Use /roll sides');
+    if (sides < 1) return new ServerMessage('Invalid number of sides (must be at least 1)');
+  }
+  let roll = Math.floor(Math.random() * sides) + 1;
+  return new ServerMessage(`${username} rolled a ${sided}die and got ${roll}.`);
+}
+
+class ServerMessage extends Message.ChatMessage {
+  constructor(message) {
+    super('OnBoard', message, true);
   }
 }
 
